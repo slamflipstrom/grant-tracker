481c8b8883b2a99bb86d289128da16d856e138c8
Heather Praiswater <heathered@gmail.com>
2014-10-07 14:18:00 -0500

    Attr accessible stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  attr_accessible :email, :password, :password_confirmation
 
   validates :password, length: { minimum: 3 }
   validates :password, confirmation: true
