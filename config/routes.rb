08e462b4149532a10f6836b7e9ffb2fb5f4de310
Diana Mkrtchyan <dianamukuch@gmail.com>
2014-10-07 14:26:30 -0500

    Create User Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 GrantTracker::Application.routes.draw do
+ 
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
 
 
