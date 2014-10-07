59032bd88ec735ace1b269938ba8e0ac470a071a
Heather Praiswater <heathered@gmail.com>
2014-10-07 11:27:55 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrantTracker::Application.config.secret_token = '734985bf9657e16b20ecb6a45b0ae075e3d718e78a3f98799de7ff4569d97e489b3d1724aaa695c35cd319d5321bb02a1c7a23f2b55405d9812b3605e9fe7bd1'
