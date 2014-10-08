a78095877d69a0733690ca5be9509827f6353b83
Heather Praiswater <heathered@gmail.com>
2014-10-08 09:47:12 -0500

    Migrate due:datetime

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141007205135) do
+ActiveRecord::Schema.define(:version => 20141008144609) do
 
   create_table "controllers", :force => true do |t|
     t.string   "user"
@@ -34,12 +34,12 @@ ActiveRecord::Schema.define(:version => 20141007205135) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
-    t.string   "due_date"
     t.text     "description"
     t.integer  "user_id"
     t.integer  "grant_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.datetime "due"
   end
 
   create_table "users", :force => true do |t|
