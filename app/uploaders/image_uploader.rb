5dd1dd41865e67b6497c91cfe1561a27589192dd
Adam Carter <adamrcarter@gmail.com>
2014-10-08 13:42:16 -0500

    wip

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,6 +8,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   
   include CarrierWave::MimeTypes
