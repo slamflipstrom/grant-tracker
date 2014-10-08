3a00326d455d71cbdd7ac9e8916dfcea3314cc97
Adam Carter <adamrcarter@gmail.com>
2014-10-08 09:35:26 -0500

    added resources for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ GrantTracker::Application.routes.draw do
   root :to => 'users#index'
   resources :user_sessions
   resources :users
+  resources :tasks
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
