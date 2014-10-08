2df5ef3f19557f10ec7040ae1d499f7d17825d19
Adam Carter <adamrcarter@gmail.com>
2014-10-08 11:31:51 -0500

    adding AWS S3

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,13 +1,14 @@
 # encoding: utf-8
 
 class FileUploader < CarrierWave::Uploader::Base
+  include CarrierWaveDirect::Uploader
 
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+
   
   include CarrierWave::MimeTypes
   process :set_content_type
@@ -15,9 +16,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
