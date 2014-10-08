0679ca743dec0d8c972d0d497d25e04bd6849a56
Adam Carter <adamrcarter@gmail.com>
2014-10-08 09:24:46 -0500

    adding file uploader to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ActiveRecord::Base
-  attr_accessible :description, :due_date, :name
+  attr_accessible :description, :due_date, :name, :file
   
   belongs_to :user
  
   belongs_to :grant
+  
+  mount_uploader :file, FileUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141007205135) do
+ActiveRecord::Schema.define(:version => 20141008142252) do
 
   create_table "controllers", :force => true do |t|
     t.string   "user"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20141007205135) do
     t.integer  "grant_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "file"
   end
 
   create_table "users", :force => true do |t|
