e34306b6b46c13551062822635868f521258e77c
Adam Carter <adamrcarter@gmail.com>
2014-10-08 11:38:38 -0500

    added @uploader to def index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
   # GET /tasks.json
   def index
     @tasks = Task.all
+    @uploader = Task.new.file
+    @uploader.success_action_redirect = new_task_url
 
     respond_to do |format|
       format.html # index.html.erb
