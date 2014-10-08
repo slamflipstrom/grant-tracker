91ef62bfdbeb1b01ac7a62bf6c11d52f9f492728
Diana Mkrtchyan <dianamukuch@gmail.com>
2014-10-08 10:36:33 -0500

    mounted uploader to grants

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -1,9 +1,11 @@
 class Grant < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :file
   
   has_many :tasks
   
   belongs_to :organization
   
   has_many :uploads
+  
+   mount_uploader :file, FileUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141008152251) do
+ActiveRecord::Schema.define(:version => 20141008153012) do
 
   create_table "controllers", :force => true do |t|
     t.string   "user"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20141008152251) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "file"
   end
 
   create_table "users", :force => true do |t|
