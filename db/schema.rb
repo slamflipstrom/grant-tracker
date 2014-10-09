7934af2703d5653b00d9fec4591a2c478fce77ec
Heather Praiswater <heathered@gmail.com>
2014-10-09 09:51:26 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141008144609) do
+ActiveRecord::Schema.define(:version => 20141008165100) do
 
   create_table "controllers", :force => true do |t|
     t.string   "user"
@@ -40,7 +40,15 @@ ActiveRecord::Schema.define(:version => 20141008144609) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.datetime "due"
-    t.string   "file"
+    t.string   "image"
+  end
+
+  create_table "uploads", :force => true do |t|
+    t.integer  "grant_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
